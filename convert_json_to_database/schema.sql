e467b2e16f565b65c52bd0d94ba5403501e29cfc
Roisin <roisin.briscoe@gmail.com>
2019-06-12 13:06:59 +1000

    individual climber page loading

--- convert_json_to_database/schema.sql
+++ convert_json_to_database/schema.sql
@@ -7,7 +7,9 @@ CREATE TABLE profiles (
     email VARCHAR(500),
     gender VARCHAR(500),
     state VARCHAR(500),
-    national_ranking VARCHAR(500),
+    lead_national_ranking VARCHAR(500),
+    boulder_national_ranking VARCHAR(500),
+    speed_national_ranking VARCHAR(500),
     world_ranking VARCHAR(500),
     website VARCHAR(500),
     facebook VARCHAR(500),
@@ -18,15 +20,15 @@ CREATE TABLE profiles (
     permission VARCHAR(500)
 );
 
+
 CREATE TABLE results (
     id SERIAL PRIMARY KEY,
     profile_id INT,
     rank VARCHAR(500),
     date VARCHAR(500),
     name VARCHAR(500),
-    gender VARCHAR(500),
     url VARCHAR(500),
-    cat_name VARCHAR(500),
+    cat_name VARCHAR(500)
 );
 
 
--- server.js
+++ server.js
@@ -4,23 +4,34 @@ const port = 8080
 // const axios = require('axios')
 const pgp = require('pg-promise')()
 var db = pgp({
-    database: 'networking',
+    database: 'athletes',
     password: process.env["zendesk_password"]
 })
-
-
-// db.any('SELECT * FROM profiles')
-//     .then(function(profile) {
-//       console.log(profile[0])
-//     })
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('home')
+    db.any('SELECT * FROM profiles')
+    .then(function(profiles) {
+        res.render('home', { profiles })
+    })
 })
+
+
+app.get('/climber', (req, res) => {
+    db.any(`SELECT * from profiles inner join results on profiles.id = results.profile_id where profile_id=${req.query.id}`)
+    .then(function(results){    
+        res.render('climber', { results })
+    })
+})
+//searh includes
+// .then(function(result){
+            
+// })
+
+// making the request but it is asynchronous therefore get back a promise.  when it completes (think promise queue) then will render the page with the information
 
 app.listen(port, () => {
     console.log(`listening ${port}`)
