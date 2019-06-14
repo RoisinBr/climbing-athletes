7777fd082a0dab54e4edfe748a567c5937208598
Roisin <roisin.briscoe@gmail.com>
2019-06-14 10:37:28 +1000

    prep for deploy

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 // const axios = require('axios')
 const pgp = require('pg-promise')()
-var db = pgp({
-    database: 'athletes',
-    password: process.env["zendesk_password"]
-})
+if (process.env.PRODUCTION) {
+    var db = pgp(process.env.DATABASE_URL)
+} else {
+    var db = pgp({
+        database: 'athletes',
+        password: process.env["zendesk_password"]
+    })
+}
 
 app.set('view engine', 'ejs')
 
